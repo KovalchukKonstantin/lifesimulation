c4ae1509c4a1a51fe68826200b679fbcd68cd044
Evgeny Taranov <taranov@jet.msk.su>
2019-06-05 18:54:27 +0300

    Code corrected during the review in Skype
    Auto-formatting
    
    Separated declaration of classes/functions and executable code
    
    Moved cell neighbors coordinate calculation into Cell as a generator
    method
    
    Other minor changes

--- noarcade.py
+++ noarcade.py
@@ -1,117 +1,126 @@
-
 class Cell:
-    def __init__(self,x,y):
+    """Represents a living cell that hold its coordinates and a number of neighbors"""
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.neighbours = 0
+        self.neighbors = 0
 
-cells = []
+    def get_neighbors(self, xmin, ymin):
+        yield self.y - ymin, self.x - xmin
+        yield self.y - ymin, self.x - xmin + 1
+        yield self.y - ymin, self.x - xmin + 2
+        yield self.y - ymin + 1, self.x - xmin
+        yield self.y - ymin + 1, self.x - xmin + 2
+        yield self.y - ymin + 2, self.x - xmin
+        yield self.y - ymin + 2, self.x - xmin + 1
+        yield self.y - ymin + 2, self.x - xmin + 2
+
 
 def create_cell_list():
-    a = input('Enter starting coordinates x,y for each cell sparated by space: ')
+    """
+    Create a plane list of intercolated x,y coordinates
+    """
+    a = input('Enter starting coordinates x,y for each cell separated by space: ')
     c = a.split()
     b = []
     for x in c:
         b.append(int(x))
     return b
-def create_cells(coordinates, cells):
-    i = 0
-    while(i<len(coordinates)):
-        cells.append(Cell(coordinates[i],coordinates[i+1]))
-        i+=2
-    return cells
-b = create_cell_list()
-cells = create_cells(b,cells)
 
 
-xar = []
-yar = []
-for t in cells:
-    xar.append(t.x)
-    yar.append(t.y)
-xmin = min(xar)
-xmax = max(xar)
-ymin = min(yar)
-ymax = max(yar)
+def create_cells(coordinates):
+    """
+    Create a list of Cells from coordinates
+
+    :param coordinates: a list of x,y coordinates
+    :return:
+    """
+    return [Cell(coordinates[i], coordinates[i + 1])
+            for i in range(0, len(coordinates), 2)]
+
 
 def make_field(cells, xmax, xmin, ymax, ymin):
-    field = [[0]*(xmax-xmin+3) for i in range (ymax-ymin+3)]
-    #for i in range(len(cells)):
-        #field[cells[i].y-ymin+1][cells[i].x-xmin+1] = cells[i]
+    field = [[0] * (xmax - xmin + 3) for _ in range(ymax - ymin + 3)]
     for t in cells:
-        field[t.y-ymin+1][t.x-xmin+1] = t
-    #for i in range(len(xar)):
-    #    field[yar[i]-ymin+1][xar[i]-xmin+1] = Cell(xar[i],yar[i])
+        field[t.y - ymin + 1][t.x - xmin + 1] = t
     return field
 
-field = make_field(cells, xmax, xmin, ymax, ymin)
 
 def modify(a):
     if isinstance(a, Cell):
-        a.neighbours+=1
+        a.neighbors += 1
     else:
-        a+=1
+        a += 1
     return a
 
-def update_field(field, cells, xmin, ymin, xmax, ymax, xar, yar):
 
-    for t in cells:
-            field[t.y-ymin][t.x-xmin] = modify(field[t.y-ymin][t.x-xmin])
-            field[t.y-ymin][t.x-xmin+1]= modify(field[t.y-ymin][t.x-xmin+1])
-            field[t.y-ymin][t.x-xmin+2] = modify(field[t.y-ymin][t.x-xmin+2])
-            field[t.y-ymin+1][t.x-xmin] = modify(field[t.y-ymin+1][t.x-xmin])
-            field[t.y-ymin+1][t.x-xmin+2] = modify(field[t.y-ymin+1][t.x-xmin+2])
-            field[t.y-ymin+2][t.x-xmin] = modify(field[t.y-ymin+2][t.x-xmin])
-            field[t.y-ymin+2][t.x-xmin + 1] = modify(field[t.y-ymin+2][t.x-xmin + 1])
-            field[t.y-ymin+2][t.x-xmin + 2] = modify(field[t.y-ymin+2][t.x-xmin + 2])
-    for t in range(len(field)):
+def update_field(field, cells, xmin, ymin, xar, yar):
+    for cell in cells:
+        for x, y in cell.get_neighbors(xmin, ymin):
+            field[y][x] = modify(field[y][x])
+    for cell in range(len(field)):
         for i in range(len(field[0])):
-            if isinstance(field[t][i], Cell):
-                if (field[t][i].neighbours<2 or field[t][i].neighbours>3):
-                    xar.remove(i+xmin-1)
-                    yar.remove(t+ymin-1)
-                    cells.remove(field[t][i])
-                    field[t][i] = 0
+            if isinstance(field[cell][i], Cell):
+                if field[cell][i].neighbors < 2 or field[cell][i].neighbors > 3:
+                    xar.remove(i + xmin - 1)
+                    yar.remove(cell + ymin - 1)
+                    cells.remove(field[cell][i])
+                    field[cell][i] = 0
             else:
-                if (field[t][i] == 3):
-                    field[t][i] = Cell(i+xmin-1,t+ymin-1)
-                    cells.append(field[t][i])
-                    xar.append(i+xmin-1)
-                    yar.append(t+ymin-1)
+                if field[cell][i] == 3:
+                    field[cell][i] = Cell(i + xmin - 1, cell + ymin - 1)
+                    cells.append(field[cell][i])
+                    xar.append(i + xmin - 1)
+                    yar.append(cell + ymin - 1)
     ymin = min(yar)
     ymax = max(yar)
     xmin = min(xar)
     xmax = max(xar)
-    field = [[0] * (xmax - xmin + 3) for i in range(ymax - ymin + 3)]
-    for t in cells:
-        field[t.y - ymin + 1][t.x - xmin + 1] = t
-    for t in field:
-        for i in t:
+    field = make_field(cells, xmax, xmin, ymax, ymin)
+    for cell in field:
+        for i in cell:
             if isinstance(i, Cell):
-                i.neighbours = 0
-            else:
-                i = 0
+                i.neighbors = 0
     return field, cells, xmin, ymin, xmax, ymax, xar, yar
+
+
 def print_field(field):
     for i in range(len(field)):
         for t in range(len(field[0])):
-            if isinstance(field[i][t],Cell):
-                print("C", end=" ")
+            if isinstance(field[i][t], Cell):
+                print("#", end=" ")
             else:
-                print(field[i][t], end=" ")
+                print(' ', end=" ")
         print(" ")
 
-print_field(field)
 
-while True:
-    t = input("Press c to continue simulation. Press q to stop simulation: ")
-    if (t == "q"):
-        print("Life simulation is over")
-        break
-    elif (t == "c"):
-        field, cells, xmin, ymin, xmax, ymax, xar, yar = update_field(field, cells, xmin, ymin, xmax, ymax, xar, yar)
-        print_field(field)
-    else:
-        print ("Please enter a valid command")
-        continue
-
+if __name__ == '__main__':
+    # initialize the state
+    b = create_cell_list()
+    cells = create_cells(b)
+    xar = []
+    yar = []
+    # compile lists of x and y coordinates
+    for t in cells:
+        xar.append(t.x)
+        yar.append(t.y)
+    xmin = min(xar)
+    xmax = max(xar)
+    ymin = min(yar)
+    ymax = max(yar)
+    field = make_field(cells, xmax, xmin, ymax, ymin)
+    print_field(field)
+    # main loop
+    while True:
+        t = input("Press c to continue simulation. Press q to stop simulation: ")
+        if (t == "q"):
+            print("Life simulation is over")
+            break
+        elif (t == "c"):
+            field, cells, xmin, ymin, xmax, ymax, xar, yar = update_field(field, cells, xmin, ymin,
+                                                                          xar, yar)
+            print_field(field)
+        else:
+            print("Please enter a valid command")
+            continue
