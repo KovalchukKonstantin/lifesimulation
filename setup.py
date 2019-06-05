4a0cc7a2caa7fdbd0826468d19f76a850bd81fe7
Evgeny Taranov <taranov@jet.msk.su>
2019-06-05 18:50:44 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='lifesimulation',
+        version='1.0',
+        packages=[''],
+        url='https://github.com/KovalchukKonstantin/lifesimulation',
+        license='',
+        author='Konstantin Kovalchuk',
+        author_email='',
+        description='Implementation of The Game of Life'
+)
