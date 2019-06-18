345fbf3ae002affecc4b99affcb2618284a53b72
KovalchukKonstantin <kokovalchuk@gmail.com>
2019-06-18 16:05:21 +0300

    Trying to solve mapcoloring problem
    No incialisation and testing so far

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mapcoloring',
+    version='First verison',
+    packages=[''],
+    url='https://github.com/KovalchukKonstantin/lifesimulation/tree/mapcoloring',
+    license='',
+    author='kostya',
+    author_email='',
+    description='Try to solve map coloring problem'
+)
